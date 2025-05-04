6ee98f3c75e16e061e6fb04b83461a50dc5fdf0a
Joseph Clement <jocl6895@colorado.edu>
2025-05-04 18:17:13 -0400

    hist making changes to do all runs in same region at once

--- macros/plot_all_dc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-root -b -q 'plot.C("../output/hists/hists_20250501_dc_49136.root","../output/hists/hists_20250501_dc_49229.root",49136,49229,1)'
-root -b -q 'plot.C("../output/hists/hists_20250501_dc_49136.root","../output/hists/hists_20250501_dc_49248.root",49136,49248,1)'
-root -b -q 'plot.C("../output/hists/hists_20250501_dc_49136.root","../output/hists/hists_20250501_dc_49265.root",49136,49265,1)'
